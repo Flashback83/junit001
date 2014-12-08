6da58a7f920878e39c345cee0b34c892eba6bc28
Flashback83 <vss.odessa@gmail.com>
2014-12-08 17:25:35 +0200

    Demo Project ver1

---
+++ src/test/java/com/serg/bloim/BlahBlahTest.java
@@ -0,0 +1,15 @@
+package com.serg.bloim;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+
+import static org.junit.Assert.*;
+
+public class BlahBlahTest {
+
+    @org.junit.Test
+    public void testMain() throws Exception {
+        System.out.println("dsfsdfa");
+        Assert.assertTrue(true);
+    }
+}
